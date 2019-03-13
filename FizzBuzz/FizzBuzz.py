21add8fc30dfb6ce975bcb25e51cf24f13d4d1db
detinin_roman <detininroman@gmail.com>
2019-03-13 22:48:09 +0300

    Added comments for functions

--- FizzBuzz/FizzBuzz.py
+++ FizzBuzz/FizzBuzz.py
@@ -1,4 +1,6 @@
 def is_int(value):
+    """ Verifies that 'value' can be converted to an int.
+    """
     try:
         int(value)
         return True
@@ -6,14 +8,20 @@ def is_int(value):
         return False
 
 def is_zero(value):
+    """ Compares 'value' with zero.
+    """
     return (abs(value - 0.) < 1e-4)
 
 def mul_5(num):
+    """ Checks the multiplicity of 5.
+    """
     if (is_int(num) == False):
         return False
     return (str(num)[-1] == '0' or str(num)[-1] == '5')
 
 def mul_3(num):
+    """ Checks the multiplicity of 3.
+    """
     if (is_int(num) == False):
         return False
     if (is_zero(num)):
@@ -30,37 +38,52 @@ def mul_3(num):
         return mul_3(summary)
 
 def fizz_buzz(numbers, output):
+    """ Matches the list of numbers with the list of expressions.
+    """
     for i in numbers:
-        if (mul_3(i) and mul_5(i)):
+        if mul_3(i) and mul_5(i):
             output.append('fizzbuzz')
-        elif (mul_3(i)):
+        elif mul_3(i):
             output.append('buzz')     
-        elif (mul_5(i)):
+        elif mul_5(i):
             output.append('fizz')
         else:
             output.append(i)
 
 def test_normal():
+    """ Normal test.
+    """
     arr, output = [1, 2, 3, 5, 15], []
     fizz_buzz(arr, output)
     print (output == [1, 2, 'buzz', 'fizz', 'fizzbuzz'])
 
 def test_big_numbers():
+    """ Test with big numbers.
+    """
     arr, output = [535555555555, 5500000000000000000000000, 10, 15, 35, 0, -2, -67, -55], []
     fizz_buzz(arr, output)
     print (output == ['fizz', 'fizz', 'fizz', 'fizzbuzz', 'fizz', 'fizzbuzz', -2, -67, 'fizz'])
 
 def test_double():
+    """ Test with a fractional number.
+    """
     arr, output = [0.1, 0.02, 0.0003], []
     fizz_buzz(arr, output)
     print (output == [0.1, 0.02, 0.0003])
 
 def test_word():
+    """ Test with a word.
+    """
     arr, output = ['Acronis', 0, 333, 450], []
     fizz_buzz(arr, output)
     print (output == ['Acronis', 'fizzbuzz', 'buzz', 'fizzbuzz'])
 
-test_normal()
-test_big_numbers()
-test_double()
-test_word()        
+def fizz_buzz_test():
+    """ Runs all tests.
+    """
+    test_normal()
+    test_big_numbers()
+    test_double()
+    test_word()
+
+fizz_buzz_test()
